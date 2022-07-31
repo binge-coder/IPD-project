54d545ebe18b4583bb537bd3ceb4c429ca1f9f99
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2022-07-31 23:09:22 +0530

    improved calculator font

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function evalFunc(){
     document.calculation.outputBox.value=eval(document.calculation.outputBox.value)
 }
 
+function backSpace() {
+    var mystring = document.calculation.outputBox.value;
+    document.calculation.outputBox.value = mystring.substring(0,(mystring.length)-1);  
+  }
+
 // Legacy Code (when i was using <input> instead of buttons)
 // function appendDifferent(objButton){
 //     if(objButton.value == 'x')
--- test.js
+++
@@ -1,4 +0,0 @@
-function backSpace() {
-  var mystring = document.calculation.outputBox.value;
-  document.calculation.outputBox.value = mystring.substring(0,(mystring.length)-1);  
-}
